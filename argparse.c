a6bb3bd877395d62d6eccb0e350ebd5ccf131880
pleaseosaur <82362420+pleaseosaur@users.noreply.github.com>
2024-04-22 21:06:05 -0700

    updating argparse function headers

--- argparse.c
+++ argparse.c
@@ -12,7 +12,22 @@
 #define TRUE  (1)
 
 /*
-* argCount is a helper function that takes in a String and returns the number of "words" in the string assuming that whitespace is the only possible delimeter.
+* argCount
+*
+* Counts the number of arguments in a given input line.
+* You may assume only whitespace is used to separate arguments.
+* argCount should be able to handle multiple whitespaces between arguments.
+*
+* Args:
+*   line: intput string containing command and arguments separated by whitespace
+*
+* Returns:
+*   The number of arguments in line.
+*   (The command itself counts as the first argument)
+*
+* Example:
+*   argCount("ls -l /home") returns 3
+*   argCount("   ls    -l   /home  ") returns 3
 */
 static int argCount(char*line)
 {
@@ -22,13 +37,29 @@ static int argCount(char*line)
 
 
 /*
+* argparse
 *
-* Argparse takes in a String and returns an array of strings from the input.
-* The arguments in the String are broken up by whitespaces in the string.
-* The count of how many arguments there are is saved in the argcp pointer
+* Parses an input line into an array of argument strings.
+* In other words, this function separates a command string into the command and its
+* arguments.
+*
+* You may assume only whitespace is used to separate arguments.
+* argparse should be able to handle multiple whitespaces between arguments.
+* The function should dynamically allocate space for the array of argument strings,
+* following the project requirements. The caller is responsible for freeing this memory.
+*
+* Args:
+*   line: input string containing command and arguments separated by whitespace
+*   argcp: stores the number of arguments in the line (argCount)
+*
+* Returns:
+*   An array of strings (one per argument). The first string is the command.
+*
+* Example:
+*   argparse("ls -l /home", &argc) might return ["ls", "-l", "/home"] and set argc to 3
+*   argparse("   ls    -l   /home  ", &argc) might return ["ls", "-l", "/home"] and set argc to 3
 */
 char** argparse(char* line, int* argcp)
 {
   //write your code
 }
-
